67a54712d51733bee697d4c754a7501ced374d08
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-06-04 11:59:55 +0530

    reorganize code

--- src/bract/dev/reload.clj
+++ src/bract/dev/reload.clj
@@ -17,19 +17,6 @@
     [bract.core Echo]))
 
 
-(defn verbose
-  "Set verbose mode to specified status (unless environment variable APP_VERBOSE is set):
-  true  - enable verbose mode
-  false - disable verbose mode
-  nil   - clear verbose mode override"
-  [status?]
-  (case status?
-    nil   (System/clearProperty "app.verbose")
-    true  (do (System/setProperty "app.verbose" "true")  (Echo/setVerbose true))
-    false (do (System/setProperty "app.verbose" "false") (Echo/setVerbose false))
-    (throw (ex-info (str "Expected argument to be true, false or nil but found " (pr-str status?)) {}))))
-
-
 (defn reinit
   "Re-initialize app"
   []
@@ -58,3 +45,16 @@
   (core-dev/stop)
   (reinit)
   (core-dev/start))
+
+
+(defn verbose
+  "Set verbose mode to specified status (unless environment variable APP_VERBOSE is set):
+  true  - enable verbose mode
+  false - disable verbose mode
+  nil   - clear verbose mode override"
+  [status?]
+  (case status?
+    nil   (System/clearProperty "app.verbose")
+    true  (do (System/setProperty "app.verbose" "true")  (Echo/setVerbose true))
+    false (do (System/setProperty "app.verbose" "false") (Echo/setVerbose false))
+    (throw (ex-info (str "Expected argument to be true, false or nil but found " (pr-str status?)) {}))))
